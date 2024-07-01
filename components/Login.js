0f18541b51702ecafed2e49e0c4cc66bf90834da
123Yusha <yr130488@googlemail.com>
2024-07-01 15:21:55 +0100

    Created login page

--- components/Login.js
+++ components/Login.js
@@ -1,6 +1,58 @@
 import * as React from 'react'
+import { TextInput, SafeAreaView, StyleSheet, Pressable, Text, View, Button} from "react-native";
+import { useState } from 'react';
+import { useForm, Controller } from 'react-hook-form';
 
 
 export default function Login() {
+    const { control, handleSubmit, formState: { errors } } = useForm({
+        defaultValues: {
+          emailAddress: '',
+          password: '',
+        }
+      });
+      const onSubmit = data => console.log(data);
+    
+      return (
+        <View>
+          <Controller
+            control={control}
+            rules={{
+             required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                placeholder="Email Address"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+            name="emailAddress"
+          />
+          {errors.emailAddress && <Text>This is required.</Text>}
+    
+          <Controller
+            control={control}
+            rules={{
+             maxLength: 100,
+             required: true
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                secureTextEntry={true}
+                placeholder="Password"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+            name="password"
+          />
+          {errors.password && <Text>This is required.</Text>}
 
+    
+          <Button title="Log In" onPress={handleSubmit(onSubmit)} />
+        </View>
+      );
 }
