29aa34e5828164c5c7d8a7ca1a1e921ab65fbf15
123Yusha <yr130488@googlemail.com>
2024-07-02 17:04:37 +0100

    working through building PlantResult page

--- components/PlantResult.js
+++ components/PlantResult.js
@@ -1,2 +1,16 @@
+import * as React from 'react'
+// import { useRoute } from '@react-navigation/native';
+import { View, Text, Stylesheet } from "react-native";
 
 
+
+export default function PlantResult({route, navigation}) {
+    // const route = useRoute()
+    const {plant} = route.params
+    console.log(route.params)
+return(
+<View>
+
+</View>
+)
+}
