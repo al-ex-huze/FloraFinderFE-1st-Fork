39aaee092fa15855d892925fdb99c8d5204e9f42
Alex Hughes <al-ex-huze@linux-desktop.Home>
2024-07-04 14:50:59 +0100

    implements postNewUser from register screen

--- contexts/Contexts.js
+++ contexts/Contexts.js
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const UserContext = createContext()
 
 export default function UserProvider (props) {
-const [user, setUser] = useState({emailAddress: "Yusha"});
+const [user, setUser] = useState({});
 return (
     <UserContext.Provider value={{user, setUser}}>
     {props.children}
