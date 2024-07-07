9345ea80b2622334657911b60b52a2d2d17d7870
Alex Hughes <al-ex-huze@linux-desktop.Home>
2024-07-07 18:06:52 +0100

    add user to collected map and changes region on load

--- components/CollectedMap.js
+++ components/CollectedMap.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { StyleSheet, View, Text, Image } from "react-native";
+import { useContext, useEffect, useState } from "react";
+import { StyleSheet, View, Text, ActivityIndicator } from "react-native";
 import MapView, {
     Marker,
     Callout,
     Heatmap,
     PROVIDER_GOOGLE,
 } from "react-native-maps";
-import { useContext, useEffect, useState } from "react";
-import { WebView } from 'react-native-webview';
+import { WebView } from "react-native-webview";
+import * as Location from "expo-location";
 
 import { UserContext } from "../contexts/Contexts";
 import { getCollectedPlantsList } from "../api";
@@ -22,12 +23,12 @@ export default function CollectedMap({ navigation }) {
     // const [heatMapPoints, setHeatMapPoints] = useState([]);
 
     const { user, setUser } = useContext(UserContext);
-    // const username = user.username;
 
+    const [isLocating, setIsLocating] = useState(true);
     const [isLoading, setIsLoading] = useState(true);
-    const [plantsArr, setPlantsArr] = useState([]);
-
+    const [location, setLocation] = useState(null);
     const [flowerIcons, setFlowerIcons] = useState(flowerIconsArr);
+    const [plantsArr, setPlantsArr] = useState([]);
 
     useEffect(() => {
         console.log("USE EFFECT in COLLECTED MAP");
@@ -47,19 +48,45 @@ export default function CollectedMap({ navigation }) {
                 ]);
             });
             */
-
-            setIsLoading(false);
         });
+
+        (async () => {
+            setIsLocating(true);
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== "granted") {
+                setErrorMsg("Permission to access location was denied");
+                return;
+            }
+            let location = await Location.getCurrentPositionAsync();
+            setLocation(location);
+            setIsLocating(false);
+        })();
+
+        setIsLoading(false);
     }, []);
 
+    if (isLoading || isLocating) {
+        return (
+            <View style={styles.activityIndicatorBackground}>
+                <ActivityIndicator
+                    style={styles.loadPage}
+                    size="large"
+                    color="#006400"
+                />
+                <Text>Loading map...</Text>
+            </View>
+        );
+    }
     return (
         <View style={styles.map_container}>
             <MapView
                 style={styles.map_view}
+                showsUserLocation={true}
+                followsUserLocation={true}
                 // provider={PROVIDER_GOOGLE}
-                initialRegion={{
-                    latitude: 51.4504791,
-                    longitude: 0.1740766,
+                region={{
+                    latitude: location.coords.latitude,
+                    longitude: location.coords.longitude,
                     latitudeDelta: 0.01,
                     longitudeDelta: 0.02,
                 }}
@@ -78,7 +105,7 @@ export default function CollectedMap({ navigation }) {
                         }
                     >
                         <Callout
-                        tooltip={true}
+                            tooltip={true}
                             onPress={() => {
                                 navigation.navigate("CollectedSingleCard", {
                                     plant: plant,
@@ -87,10 +114,10 @@ export default function CollectedMap({ navigation }) {
                         >
                             <View style={styles.callout_container}>
                                 <View style={styles.callout_template}>
-                                        <WebView
-                                            style={styles.callout_image}
-                                            source={{ uri: plant.image }}
-                                        />
+                                    <WebView
+                                        style={styles.callout_image}
+                                        source={{ uri: plant.image }}
+                                    />
                                     <View style={styles.text_container_1}>
                                         <Text style={styles.text_1}>
                                             {plant.speciesName}
@@ -132,7 +159,7 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
         borderRadius: 10,
-        overflow: "hidden"
+        overflow: "hidden",
     },
     callout_template: {
         width: 100,
@@ -149,6 +176,7 @@ const styles = StyleSheet.create({
         position: "absolute",
         width: "100%",
         height: 14,
+        paddingHorizontal: "7%",
         backgroundColor: "rgba(0,0,0, 0.3)",
         borderTopLeftRadius: 10,
         borderTopRightRadius: 10,
@@ -158,6 +186,7 @@ const styles = StyleSheet.create({
         width: "100%",
         height: 14,
         bottom: 0,
+        paddingHorizontal: "7%",
         backgroundColor: "rgba(0,0,0, 0.3)",
         borderBottomLeftRadius: 10,
         borderBottomRightRadius: 10,
@@ -173,5 +202,14 @@ const styles = StyleSheet.create({
         fontSize: 10,
         letterSpacing: 0.25,
         color: "white",
+    },
+    loadPage: {
+        backgroundColor: "#CCFFCC",
+    },
+    activityIndicatorBackground: {
+        backgroundColor: "#CCFFCC",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
     },
 });
