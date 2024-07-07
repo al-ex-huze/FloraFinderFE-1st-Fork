3bb4858ecfbfdb079bc0f4d15efc7be78ad95f2e
Alex Hughes <al-ex-huze@linux-desktop.Home>
2024-07-07 14:06:26 +0100

    allows navigating to single card from map callout and post preview logic

--- components/CollectedMap.js
+++ components/CollectedMap.js
@@ -1,9 +1,11 @@
 import React from "react";
-import {
-    StyleSheet,
-    View,
-} from "react-native";
-import MapView, { Marker, Heatmap, PROVIDER_GOOGLE } from "react-native-maps";
+import { StyleSheet, View, Text, Image } from "react-native";
+import MapView, {
+    Marker,
+    Callout,
+    Heatmap,
+    PROVIDER_GOOGLE,
+} from "react-native-maps";
 import { useContext, useEffect, useState } from "react";
 
 import { UserContext } from "../contexts/Contexts";
@@ -87,7 +89,40 @@ export default function CollectedMap({ navigation }) {
                                 Math.floor(Math.random() * flowerIcons.length)
                             ]
                         }
-                    />
+                    >
+                        <Callout
+                            onPress={() => {
+                                navigation.navigate("CollectedSingleCard", {
+                                    plant: plant,
+                                });
+                            }}
+                        >
+                            <Text style={styles.callout_text}>
+                                CALLOUT TEST
+                            </Text>
+                            <View style={styles.callout_container}>
+                                <View style={styles.card_template}>
+                                    <Text>
+                                        <Image
+                                            // style={styles.card_image}
+                                            style={{ height: 100, width: 100 }}
+                                            source={{ uri: plant.image }}
+                                        />
+                                    </Text>
+                                    <View style={styles.text_container_1}>
+                                        <Text style={styles.text_1}>
+                                            {plant.speciesName}
+                                        </Text>
+                                    </View>
+                                    <View style={styles.text_container_2}>
+                                        <Text style={styles.text_2}>
+                                            {plant.plantId}
+                                        </Text>
+                                    </View>
+                                </View>
+                            </View>
+                        </Callout>
+                    </Marker>
                 ))}
 
                 {/* {heatMapPoints[0] ? (
@@ -97,7 +132,6 @@ export default function CollectedMap({ navigation }) {
                         points={heatMapPoints}
                     ></Heatmap>
                 ) : null} */}
-
             </MapView>
         </View>
     );
@@ -114,7 +148,9 @@ const styles = StyleSheet.create({
         width: "100%",
         height: "100%",
     },
-
+    callout_text: {
+        color: "black",
+    },
     text: {
         fontSize: 24,
         fontWeight: "bold",
@@ -129,5 +165,55 @@ const styles = StyleSheet.create({
         elevation: 3,
         backgroundColor: "#006400",
         margin: 10,
+    },
+
+    callout_container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        height: 128,
+    },
+    card_template: {
+        width: "100%",
+        height: 128,
+        boxShadow: "10px 10px 17px -12px rgba(0,0,0,0.75)",
+    },
+    card_image: {
+        width: 128,
+        height: 128,
+        // borderRadius: 10,
+    },
+    text_container_1: {
+        position: "absolute",
+        width: "100%",
+        height: 32,
+        padding: 4,
+        backgroundColor: "rgba(0,0,0, 0.3)",
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+    },
+    text_container_2: {
+        position: "absolute",
+        width: "100%",
+        height: 32,
+        bottom: 0,
+        padding: 4,
+        backgroundColor: "rgba(0,0,0, 0.3)",
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10,
+        alignItems: "flex-end",
+    },
+    text_1: {
+        fontSize: 22,
+        lineHeight: 24,
+        fontWeight: "bold",
+        letterSpacing: 0.25,
+        color: "white",
+    },
+    text_2: {
+        fontSize: 16,
+        lineHeight: 18,
+        letterSpacing: 0.25,
+        color: "white",
     },
 });
