bcdaf7b3ad1f17a4df4e74632d41285233751fd5
123Yusha <yr130488@googlemail.com>
2024-07-08 16:31:18 +0100

    added styling consistency to opening page, login, register, home and collectedlist

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StyleSheet, Text, View } from "react-native";
 import * as React from "react";
-
 import UserProvider from "./contexts/Contexts";
 import { NavigationContainer } from "@react-navigation/native";
 import { TabNavigator } from "./components/navigators/TabNavigator";
+import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
 
 export default function App() {
     // see /components/navigators
+    let [fontsLoaded] = useFonts({
+        Inter_900Black,
+      });
+    
+      if (!fontsLoaded) {
+        return null;
+      }
     return (
         <UserProvider>
             <NavigationContainer>
--- components/Register.js
+++ components/Register.js
@@ -1,57 +1,69 @@
-import * as React from 'react'
-import { TextInput, SafeAreaView, StyleSheet, Pressable, Text, View, Button, Alert} from "react-native";
-import { useState } from 'react';
+import * as React from 'react';
+import { TextInput, StyleSheet, Pressable, Text, View, ImageBackground, Alert, Image, ScrollView } from 'react-native';
 import { useForm, Controller } from 'react-hook-form';
 import { postNewUser } from '../api';
 
-
-
-
+const backgroundLeaf = require('../assets/backgroundtest.jpg');
+const logo = require('../assets/FloraFinderLogo.png');
 
 export default function Register({ navigation }) {
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    defaultValues: {
+      emailAddress: '',
+      password: '',
+      username: '',
+      firstName: '',
+      lastName: ''
+    }
+  });
 
-    const { control, handleSubmit, formState: { errors } } = useForm({
-        defaultValues: {
-          emailAddress: '',
-          password: '',
-          username: '',
-          firstName: '',
-          lastName: ''
-        }
+  const onSubmit = (data) => {
+    const newUser = {
+      username: data.username,
+      name: data.firstName + ' ' + data.lastName,
+      email: data.emailAddress,
+      password: data.password
+    };
+
+    postNewUser(newUser)
+      .then((user) => {
+        Alert.alert('Registration complete!', 'Please login to your account.', [
+          {
+            text: 'Login',
+            onPress: () => navigation.navigate('Login'),
+            style: 'default',
+          },
+        ]);
+      })
+      .catch((error) => {
+        console.log(error, 'Registration Failed');
       });
+  };
+
+  return (
+    <ScrollView
+      contentContainerStyle={styles.background}
+      showsVerticalScrollIndicator={false}
       
-      const onSubmit = (data) => {
-        console.log(data, "data")
-        const newUser = {username: data.username, name: data.firstName + " " + data.lastName, email: data.emailAddress, password: data.password};
-        console.log(newUser);
-        postNewUser(newUser)
-        .then((user) => {
-          Alert.alert('Registration complete!', 'Please login to your account.', [
-            {
-              text: 'Login',
-              onPress: () => navigation.navigate('Login'),
-              style: 'default',
-            },
-          ]);
-        })
-        .catch((error) => {
-          console.log(error, "Registration Failed")
-        })
-      };
-
-     
-   
-
-return (
+    >
+      <ImageBackground
+        source={backgroundLeaf}
+        style={styles.backgroundImage}
+        resizeMode="stretch"
+      >
+        <View style={styles.overlay}></View>
         <View style={styles.container}>
-          <Text style={styles.heading}>Create An Account</Text>
-          <Text>Enter your email address:</Text>
+        <View style={styles.logoContainer}>
+          <Image source={logo} style={styles.logo} />
+        </View>
+          <Text style={styles.heading}>Register</Text>
+
+          <Text style={styles.labelContainerText}>Enter your email address:</Text>
           <Controller
             control={control}
             rules={{
-             required: true,
-             pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-             
+              required: true,
+              pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
             }}
             render={({ field: { onChange, onBlur, value } }) => (
               <TextInput
@@ -65,14 +77,15 @@ return (
             name="emailAddress"
           />
           {errors.emailAddress && <Text style={styles.errorText}>Invalid Email Address.</Text>}
-          <Text>Create a password:</Text>
+
+          <Text style={styles.labelContainerText}>Create a password:</Text>
           <Controller
             control={control}
             rules={{
-             maxLength: 20,
-             minLength: 5,
-             required: true,
-             pattern: /\w/
+              maxLength: 20,
+              minLength: 5,
+              required: true,
+              pattern: /\w/
             }}
             render={({ field: { onChange, onBlur, value } }) => (
               <TextInput
@@ -87,12 +100,13 @@ return (
             name="password"
           />
           {errors.password && <Text style={styles.errorText}>Password must be 5-20 characters long and only contain numbers, letters and underscores. </Text>}
-          <Text>Enter a username:</Text>
+
+          <Text style={styles.labelContainerText}>Enter a username:</Text>
           <Controller
             control={control}
             rules={{
-             maxLength: 100,
-             required: true
+              maxLength: 100,
+              required: true
             }}
             render={({ field: { onChange, onBlur, value } }) => (
               <TextInput
@@ -105,13 +119,14 @@ return (
             )}
             name="username"
           />
-          {errors.username && <Text>This is required.</Text>}
-          <Text>Enter your first name:</Text>
+          {errors.username && <Text style={styles.errorText}>This is required.</Text>}
+
+          <Text style={styles.labelContainerText}>Enter your first name:</Text>
           <Controller
             control={control}
             rules={{
-             maxLength: 100,
-             required: true
+              maxLength: 100,
+              required: true
             }}
             render={({ field: { onChange, onBlur, value } }) => (
               <TextInput
@@ -124,13 +139,14 @@ return (
             )}
             name="firstName"
           />
-          {errors.firstName && <Text>This is required.</Text>}
-          <Text>Enter your last name:</Text>
+          {errors.firstName && <Text style={styles.errorText}>This is required.</Text>}
+
+          <Text style={styles.labelContainerText}>Enter your last name:</Text>
           <Controller
             control={control}
             rules={{
-             maxLength: 100,
-             required: true
+              maxLength: 100,
+              required: true
             }}
             render={({ field: { onChange, onBlur, value } }) => (
               <TextInput
@@ -143,66 +159,95 @@ return (
             )}
             name="lastName"
           />
-          {errors.lastName && <Text>This is required.</Text>}
-    
+          {errors.lastName && <Text style={styles.errorText}>This is required.</Text>}
+
           <Pressable style={styles.button} title="Submit" onPress={handleSubmit(onSubmit)}>
             <Text style={styles.buttonText}>Create An Account</Text>
           </Pressable>
         </View>
-      );
+      </ImageBackground>
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
+  logo: {
+    height: 250,
+    resizeMode: 'contain',
+  },
+  background: {
+    flexGrow: 1,
+  },
+  backgroundImage: {
+    flexGrow: 1,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logoContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    alignItems: 'center',
+  },
+  
+    overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+  },
   container: {
-      flex: 1, // makes sure the colour takes up the whole screen
-      backgroundColor: "#CCFFCC", // Kate colour change
-      alignItems: "center", // horizontal alex
-      justifyContent: "center", // vertical alex
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 20,
   },
+
   button: {
-      alignItems: "center",
-      justifyContent: "center",
-      paddingVertical: 12,
-      paddingHorizontal: 32,
-      borderRadius: 4,
-      elevation: 3,
-      backgroundColor: "#006400",
-      width: "50%", // percentages need to be in in quotes alex
-      margin: 12,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: '#006400',
+    width: '50%', // percentages need to be in quotes
+    margin: 12,
   },
-  text: {
-      fontSize: 16,
-      lineHeight: 21,
-      fontWeight: "bold",
-      letterSpacing: 0.25,
-      color: "white",
+  heading: {
+    color: "#006400",
+    marginBottom: 10,
+    fontFamily: 'Inter_900Black', 
+    fontSize: 25,
+    paddingTop: 150,
+},
+  textInput: {
+    backgroundColor: "white",
+    height: 40,
+    width: 250,
+    borderWidth: 2,
+    borderRadius: 5,
+    borderStyle: "solid",
+    borderColor: "#006400",
+    marginVertical: 10,
+    paddingHorizontal: 10,
+},
+  buttonText: {
+    color: 'white',
   },
-  image: {
-      width: '80%', 
-      resizeMode: 'contain',
+  errorText: {
+    color: 'red',
   },
-  buttonText : {
-    color: "white",
+  labelContainerText: {
+    // backgroundColor: "white",
+    // borderWidth: 2,
+    // borderRadius: 5,
+    // borderStyle: "solid",
+    // borderColor: "#006400",
+    // padding: 5,
+    fontWeight: "bold",
+    alignSelf: 'flex-start',
+    marginTop: 10,
+    marginBottom: -4,
 },
-heading: {
-  fontSize: 25,
-  fontWeight: "bold",
-  color: "#006400",
-  margin: 12,
-},
-textInput: {
-backgroundColor: "white",
-height: 25,
-width: 200,
-borderWidth: 2,
-borderRadius: 5,
-borderStyle: "solid",
-borderColor: "#006400",
-},
-buttonText : {
-  color: "white",
-},
-errorText: {
-  color: "red"
-}
 });
