81861c810b4322096c1ba3266e19110f55a5136a
Alex Hughes <al-ex-huze@linux-desktop.Home>
2024-07-09 11:32:40 +0100

    adds formatName and tests and implements it in PlantResult

---
+++ utils/__tests__/formatName.test.js
@@ -0,0 +1,30 @@
+const { formatName } = require("../formatName")
+const testCollection = require("../../test-data/collection")
+
+describe("formatName", () => {
+    test("Takes a string and returns with each word capitalized", () => {
+        const input = "Lemon geranium";
+
+        const expected = "Lemon Geranium"
+
+        const actual = formatName(input);
+
+        expect(actual).toBe(expected)
+    })
+    test("Takes a string of multiple words and returns with each word capitalized", () => {
+        const input = "The great big australian spider eating plant";
+
+        const expected = "The Great Big Australian Spider Eating Plant"
+
+        const actual = formatName(input);
+
+        expect(actual).toBe(expected)
+    })
+    test("Doesnt change input (string)", () => {
+        const input = "Lemon geranium";
+
+        const actual = formatName(input);
+
+        expect(input).toBe("Lemon geranium")
+    })
+})
